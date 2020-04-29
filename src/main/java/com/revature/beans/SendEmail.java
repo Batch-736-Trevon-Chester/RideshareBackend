3355e7370e025f5028a2ea4be6502adcdcd9b265
Harvey Herring <harvey_herring@yahoo.com>
2020-04-29 10:09:22 -0400

    added email feature

--- src/main/java/com/revature/beans/SendEmail.java
+++ src/main/java/com/revature/beans/SendEmail.java
@@ -64,6 +64,49 @@ public class SendEmail {
         }
     }
     
+    private static final String REACTIVATE_ACC_EMAIL_SUBJECT = "Your account has been disabled";
+    private static final String REACTIVATE_ACC_EMAIL_ADDRESS = "admin@admin.com";
+    private static final String REACTIVATE_ACC_EMAIL = "In order to reactivate your account, please contact an administrator at: \n";
+    public static boolean sendEmail(String recipientEmail) {
+    	Properties prop = new Properties();
+        prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.port", "587"); // default port 25
+        prop.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(prop, null);
+        try {
+        	Message msg = new MimeMessage(session);
+		
+			// from
+            msg.setFrom(new InternetAddress(EMAIL_FROM));
+			// to
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(recipientEmail, false));
+			// subject
+            msg.setSubject(REACTIVATE_ACC_EMAIL_SUBJECT);
+			
+			// content
+            msg.setText(REACTIVATE_ACC_EMAIL + REACTIVATE_ACC_EMAIL_ADDRESS);
+			
+            msg.setSentDate(new Date());
+			// Get SMTPTransport
+            SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
+			
+			// connect
+            t.connect(SMTP_SERVER, USERNAME, PASSWORD);
+			
+			// send
+            t.sendMessage(msg, msg.getAllRecipients());
+//            loggy.info("Email was sent to alert for a password update.");
+            t.close();
+            return true;
+        } catch (MessagingException e) {
+//        	loggy.error("Email failed to send", e);
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
     public static int generateOTP() {
     	int max = 999999;
     	int min = 100000;
